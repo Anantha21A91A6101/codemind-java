a843f789355901950b9e3aa2c64a3139c50638f7
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-09-11 16:54:56 +0530

    import java.util.Scanner; class Fahrenheit {     public static void main(String args[])     {         int c;         float f;         Scanner sc=new Scanner(System.in);         c=sc.nextInt();         f=(float)(c*1.8+32);         System.out.format("%.2f",f);         sc.close();     } }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Fahrenheit
+{
+    public static void main(String args[])
+    {
+        int c;
+        float f;
+        Scanner sc=new Scanner(System.in);
+        c=sc.nextInt();
+        f=(float)(c*1.8+32);
+        System.out.format("%.2f",f);
+        sc.close();
+    }
+}
