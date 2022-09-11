5a09cb78dfc481d6e2b0f8e3ab5bcdb04804c264
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-09-11 16:31:46 +0530

    import java.util.Scanner; class Divisible {     public static void main(String args[])     {         int n,k,sum=0,i;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int[] a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         k=sc.nextInt();         for(i=0;i<n;i++)         {             if(a[i]==k)             {                 sum=sum+1;             }         }         System.out.println(sum);         sc.close();     } }

---
+++ count_of_given_element_in_an_array.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Divisible
+{
+    public static void main(String args[])
+    {
+        int n,k,sum=0,i;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int[] a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        k=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(a[i]==k)
+            {
+                sum=sum+1;
+            }
+        }
+        System.out.println(sum);
+        sc.close();
+    }
+}
