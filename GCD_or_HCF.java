d5c700729dc0d8329308cc00eeb3e91dbc67ecca
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-09-12 20:07:54 +0530

    import java.util.Scanner; class Lcm {     public static void main(String atgs[])     {         int a,b,i,gcd=0,lcm;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         for(i=1;i<=a&&i<=b;i++)         {             if(a%i==0&&b%i==0)             {                 gcd=i;             }         }         System.out.println(gcd);         sc.close();     } }

---
+++ GCD_or_HCF.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Lcm
+{
+    public static void main(String atgs[])
+    {
+        int a,b,i,gcd=0,lcm;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        for(i=1;i<=a&&i<=b;i++)
+        {
+            if(a%i==0&&b%i==0)
+            {
+                gcd=i;
+            }
+        }
+        System.out.println(gcd);
+        sc.close();
+    }
+}
