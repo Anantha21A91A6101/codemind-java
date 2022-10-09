874dcc53edd7b52a429a7a82dc8afd8f7c0651ef
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-10-09 16:26:51 +0530

    import java.util.Scanner; class Even {     public static void main(String args[])     {         int n,i,j,sum=0,m;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         m=sc.nextInt();         int[][] a=new int[n][m];         for(i=0;i<n;i++)         {            for(j=0;j<m;j++)            {                a[i][j]=sc.nextInt();            }         }         for(i=0;i<n;i++)         {             for(j=0;j<m;j++)             {                 if(a[i][j]%2==0)                 {                     sum=sum+a[i][j];                 }             }         }         System.out.print(sum+" ");         sum=0;         for(i=0;i<n;i++)         {             for(j=0;j<m;j++)             {                 if(a[i][j]%2!=0)                 {                     sum=sum+a[i][j];                 }             }         }         System.out.print(sum);         sc.close();     } }

---
+++ Sum_of_even_and__odd_elements_in_a_matrix.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+class Even
+{
+    public static void main(String args[])
+    {
+        int n,i,j,sum=0,m;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        m=sc.nextInt();
+        int[][] a=new int[n][m];
+        for(i=0;i<n;i++)
+        {
+           for(j=0;j<m;j++)
+           {
+               a[i][j]=sc.nextInt();
+           }
+        }
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                if(a[i][j]%2==0)
+                {
+                    sum=sum+a[i][j];
+                }
+            }
+        }
+        System.out.print(sum+" ");
+        sum=0;
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                if(a[i][j]%2!=0)
+                {
+                    sum=sum+a[i][j];
+                }
+            }
+        }
+        System.out.print(sum);
+        sc.close();
+    }
+}
