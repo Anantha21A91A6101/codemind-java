47544b443e5a69b33531ac6b2bb8f8e4844434a3
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-10-09 16:31:25 +0530

    import java.util.Scanner; class Even {     public static void main(String args[])     {         int n,i,j,sum=0,m,c=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         m=sc.nextInt();         int[][] a=new int[n][m];         for(i=0;i<n;i++)         {            for(j=0;j<m;j++)            {                a[i][j]=sc.nextInt();            }         }         for(j=0;j<m;j++)         {             sum=0;             for(i=0;i<n;i++)             {                 sum=sum+a[i][j];             }             if(c<=sum)             {                 c=sum;             }         }         System.out.print(c);         sc.close();     } }

---
+++ Maximum_sum_of_column_wise_sum_of_a_matrix.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Even
+{
+    public static void main(String args[])
+    {
+        int n,i,j,sum=0,m,c=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        m=sc.nextInt();
+        int[][] a=new int[n][m];
+        for(i=0;i<n;i++)
+        {
+           for(j=0;j<m;j++)
+           {
+               a[i][j]=sc.nextInt();
+           }
+        }
+        for(j=0;j<m;j++)
+        {
+            sum=0;
+            for(i=0;i<n;i++)
+            {
+                sum=sum+a[i][j];
+            }
+            if(c<=sum)
+            {
+                c=sum;
+            }
+        }
+        System.out.print(c);
+        sc.close();
+    }
+}
