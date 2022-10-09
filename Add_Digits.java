20d534a49bfea3c5efdb0ad26e27f81cf3a93411
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-10-09 15:19:25 +0530

    import java.util.Scanner; class Prefect {     public static void main(String args[])     {         int n,sum=0,r;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         while(n>0)         {             r=n%10;             sum=sum+r;             n=n/10;             if(n==0 && sum>9)             {                 n=sum;                 sum=0;             }         }         System.out.print(sum);         sc.close();     } }

---
+++ Add_Digits.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Prefect
+{
+    public static void main(String args[])
+    {
+        int n,sum=0,r;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        while(n>0)
+        {
+            r=n%10;
+            sum=sum+r;
+            n=n/10;
+            if(n==0 && sum>9)
+            {
+                n=sum;
+                sum=0;
+            }
+        }
+        System.out.print(sum);
+        sc.close();
+    }
+}
