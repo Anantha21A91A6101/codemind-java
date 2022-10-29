5c2fad193c5a4d2f70d6f2d1d27a79eed0b99d57
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-10-29 11:37:26 +0530

    import java.util.Scanner; class Count {     public static void main(String args[])     {         int n,i;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int[] a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         if(n%2==0)         {             for(i=0;i<n;i++)             {                 System.out.print(a[i]+" ");             }         }         else         {             for(i=0;i<n;i++)             {                 System.out.print(a[i]+" ");             }             System.out.print("0");         }     } }

---
+++ Elements_with_pairs.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Count
+{
+    public static void main(String args[])
+    {
+        int n,i;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int[] a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        if(n%2==0)
+        {
+            for(i=0;i<n;i++)
+            {
+                System.out.print(a[i]+" ");
+            }
+        }
+        else
+        {
+            for(i=0;i<n;i++)
+            {
+                System.out.print(a[i]+" ");
+            }
+            System.out.print("0");
+        }
+    }
+}
