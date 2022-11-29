9e34a531ceab268f99b2dae0a82c0dee95c48412
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-11-29 19:44:19 +0530

    import java.util.Scanner; class Slice {     public static void main(String args[])     {         String s,s1;         Scanner sc=new Scanner(System.in);         s=sc.nextLine();         System.out.print(s.toLowerCase());     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Slice
+{
+    public static void main(String args[])
+    {
+        String s,s1;
+        Scanner sc=new Scanner(System.in);
+        s=sc.nextLine();
+        System.out.print(s.toLowerCase());
+    }
+}
